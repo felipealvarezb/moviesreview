c064bfbbcc3e15a3d724756f7c3c33fb64cd55c3
felipealvarezb <felipealvarezb1@gmail.com>
2022-03-03 17:35:02 -0500

    Actualizando proyecto de django

---
+++ movie/models.py
@@ -0,0 +1,10 @@
+from email.mime import image
+from pickle import TRUE
+from turtle import title
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100) 
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to = 'media/images')
+    url = models.URLField(blank=TRUE)
